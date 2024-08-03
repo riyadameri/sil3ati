f9d0cca27699cc859ebaf35585c194708406ff6a
ISLAM Boukhendeg <boukhendegnour@gmail.com>
2024-08-03 17:43:06 +0100

    edit islam login

--- sil3ati/frontend/webapp/src/app/component/account/login/login.component.ts
+++ sil3ati/frontend/webapp/src/app/component/account/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { CreateService } from '../../../services/create.service';
+import { Router } from '@angular/router';
+import { error } from 'console';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,37 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  user = {
+    email: '',
+    password: ''
+  }
+
+  showpass() {
+    let showpassword = document.querySelector(".show") as HTMLInputElement;
+    let checkbox = document.querySelector("#togglePassword") as HTMLInputElement;
+
+    checkbox.addEventListener("change", () => {
+        if (checkbox.checked) {
+            showpassword.type = "text";
+        } else {
+            showpassword.type = "password";
+        }
+    });
+  }
+
+  constructor( private http : CreateService , private router: Router){}
+
+  checkpass(){
+    this.http.checkpassword(this.user)
+      .subscribe((res) => {
+        console.log(res);
+      },
+      (error)=>{
+        console.error('error checking password',error);
+      }
+    );
+  }
+
+  
 
 }
