d3a29471271aaf4aba9e8634eac184c8a78b3e13
riad <ameririyadyousef@gmail.com>
2024-08-04 22:12:52 +0100

    geting product data and posting product data

--- sil3ati/back-end/router/product.js
+++ sil3ati/back-end/router/product.js
@@ -3,18 +3,13 @@ const router = express.Router();
 const Product = require('../models/products');
 const authMiddleware = require('../authMiddleware');
 
-
-router.post('/addProduct',authMiddleware,(req,res)=>{
-  const cnf = authMiddleware ;
-  console.log(authMiddleware)
-  const { name, price, image, description, category, quantity, } = req.body;
-
+router.post('/addProduct', authMiddleware, (req, res) => {
+  const { name, price, image, description, category, quantity } = req.body;
   if (!name || !price || !image || !description || !category || !quantity) {
     return res.status(422).json({ error: "Please add all the fields" });
   }
-
   try {
-
+    const supplierId = req.user.id; // Extract supplierId from req.user
     const newProduct = new Product({
       name,
       price,
@@ -23,17 +18,50 @@ router.post('/addProduct',authMiddleware,(req,res)=>{
       category,
       quantity,
       date: new Date(),
-      supplierId : cnf.userId
-
+      supplierId: supplierId // Set supplierId from the token
     });
-    newProduct.save();
-    return res.status(200).json({ message: "Product added successfully" });
+    newProduct.save()
+      .then(() => res.status(200).json({ message: "Product added successfully" }))
+      .catch(err => res.status(500).json({ error: "Something went wrong" }));
 
   } catch (err) {
     console.log(err);
     return res.status(500).json({ error: "Something went wrong" });
   }
-
 });
+router.delete(
+  '/deleteProduct/:id',
+  authMiddleware,
+  async (req, res) => {
+    const { id } = req.params;
+    try {
+      const foundProduct = await Product.findById(id);
+      if (!foundProduct) {
+        return res.status(404).json({ error: "Product not found" });
+      }
+      await Product.findByIdAndDelete(id);
+      return res.status(200).json({ message: "Product deleted successfully" });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ error: "Server error" });
+    }
+  }
+)
 
+router.get(
+  '/getProducts',
+  authMiddleware,
+  async (req, res) => {
+    try {
+      const products = await Product.find();
+      return res.status(200).json({
+        message: "Products fetched successfully",
+        data: products
+      });
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json({ error: "Server error" });
+    }
+  }
+)
 module.exports = router;
